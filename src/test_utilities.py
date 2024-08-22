37668d4342517927e848aa44bb9406fcddab24b1
chuck21619 <charles.ward.johnston@gmail.com>
2024-08-22 17:46:43 -0400

    add regex searches

--- src/test_utilities.py
+++ src/test_utilities.py
@@ -12,11 +12,22 @@ class TestUtilities(unittest.TestCase):
             TextNode("code block", text_type_code),
             TextNode(" word", text_type_text)
         ]
-        
+
         self.assertEqual(new_nodes[0], expected_nodes[0])
         self.assertEqual(new_nodes[1], expected_nodes[1])
         self.assertEqual(new_nodes[2], expected_nodes[2])
 
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        value = extract_markdown_images(text)
+        expected_value = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(value, expected_value)
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        value = extract_markdown_links(text)
+        expected_value = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(value, expected_value)
         
 if __name__ == "__main__":
     unittest.main()
--- src/utilities.py
+++ src/utilities.py
@@ -1,9 +1,13 @@
 from textnode import *
+import re
 
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-
     new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != text_type_text:
